fd75e75c5670f4c7c514a0285f38638d39df3d3d
Elias Khsheibun <elias3@gmail.com>
2018-08-18 15:12:55 +0200

    unwarped images

--- src/calibration.py
+++ src/calibration.py
@@ -3,6 +3,7 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import glob
+import os
 
 
 def calculateCameraPoints(images, grid_m, grid_n):
@@ -18,7 +19,7 @@ def calculateCameraPoints(images, grid_m, grid_n):
     for imgName in images:
         # read each image
         img = mpimg.imread(imgName)
-       # print(imgName)
+        # print(imgName)
         # convert to grayscale
         gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
@@ -31,12 +32,12 @@ def calculateCameraPoints(images, grid_m, grid_n):
             objWorldPoints.append(objPoints)
 
             # uncomment to save
-            #plainName = imgName.split("/")[2]
+            # plainName = imgName.split("/")[2]
            # draw and display the corners
             # img = cv2.drawChessboardCorners(
             # img, (grid_m, grid_n), corners, ret)
 
-            #mpimg.imsave("../output_images/calibration/"+plainName, img)
+            # mpimg.imsave("../output_images/calibration/"+plainName, img)
            # plt.imshow(img)
            #     cv2.imshow('corners', cv2.cvtColor(
           #       img, cv2.COLOR_RGB2BGR))
@@ -54,6 +55,51 @@ def calcUndistort(img, objpoints, imgpoints):
     return dst
 
 
+def calcMtxDist(img, objpoints, imgpoints):
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
+        objpoints, imgpoints, img.shape[1::-1], None, None)
+    return mtx, dist
+
+
+# Define a function that takes an image, number of x and y points,
+# camera matrix and distortion coefficients
+def corners_unwarp(img, nx, ny, mtx, dist):
+    # Use the OpenCV undistort() function to remove distortion
+    undist = cv2.undistort(img, mtx, dist, None, mtx)
+    # Convert undistorted image to grayscale
+    gray = cv2.cvtColor(undist, cv2.COLOR_BGR2GRAY)
+
+    # Search for corners in the grayscaled image
+    ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
+
+    if ret == True:
+        # If we found corners, draw them! (just for fun)
+        cv2.drawChessboardCorners(undist, (nx, ny), corners, ret)
+        # Choose offset from image corners to plot detected corners
+        # This should be chosen to present the result at the proper aspect ratio
+        # My choice of 100 pixels is not exact, but close enough for our purpose here
+        offset = 100  # offset for dst points
+        # Grab the image shape
+        img_size = (gray.shape[1], gray.shape[0])
+
+        # For source points I'm grabbing the outer four detected corners
+        src = np.float32([corners[0], corners[nx-1],
+                          corners[-1], corners[-nx]])
+        # For destination points, I'm arbitrarily choosing some points to be
+        # a nice fit for displaying our warped result
+        # again, not exact, but close enough for our purposes
+        dst = np.float32([[offset, offset], [img_size[0]-offset, offset],
+                          [img_size[0]-offset, img_size[1]-offset],
+                          [offset, img_size[1]-offset]])
+        # Given src and dst points, calculate the perspective transform matrix
+        M = cv2.getPerspectiveTransform(src, dst)
+        # Warp the image using OpenCV warpPerspective()
+        warped = cv2.warpPerspective(undist, M, img_size)
+        # Return the resulting image and matrix
+        return warped, M
+    return None, None
+
+
 def test_distortion():
     # Read
     images = glob.glob('../camera_cal/calibration*.jpg')
@@ -66,9 +112,36 @@ def test_distortion():
         plainName = imgName.split("/")[2]
         cv2.imwrite('../output_images/undistorted/'+plainName, undistorted)
     # cv2.cvtColor(undistorted, cv2.COLOR_RGB2BGR))
-    #cv2.imshow('undistorted', undistorted)
+    # cv2.imshow('undistorted', undistorted)
    # cv2.waitKey(0)
 
+
+def test_unwarp():
+    # Read
+    images = glob.glob('../camera_cal/calibration*.jpg')
+    nx = 9
+    ny = 6
+    objpoints, imgpoints = calculateCameraPoints(images, nx, ny)
+    isFirst = True
+    mtx = None
+    dist = None
+    for imgName in images:
+        print(imgName)
+        img = cv2.imread(imgName)
+        if isFirst:
+            mtx, dist = calcMtxDist(np.copy(img), objpoints, imgpoints)
+            print(mtx)
+            print(dist)
+            isFirst = False
+       # cv2.imshow('image2', image)
+      #  cv2.waitKey(0)
+        unwarped, _ = corners_unwarp(np.copy(img), nx, ny, mtx, dist)
+        if unwarped is not None:
+            plainName = imgName.split("/")[2]
+            cv2.imwrite('../output_images/unwarped/'+plainName, unwarped)
+
+
+test_unwarp()
 
 # test_distortion()
 
